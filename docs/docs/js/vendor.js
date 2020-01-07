10caa8c928138494d15378f76f7da7b0fa4c9da0
tomas.pavel@etnetera.cz <tomas.pavel@etnetera.cz>
2020-01-07 23:33:23 +0100

    docs

--- docs/docs/js/vendor.js
+++ docs/docs/js/vendor.js
@@ -185,7 +185,7 @@
       });
     }
   };
-  var port = ar.port || 9486;
+  var port = ar.port || 9485;
   var host = br.server || window.location.hostname || 'localhost';
 
   var connect = function connect() {
